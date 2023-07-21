feeed59cbef8288766d3aa01fcae46c5ecda5163
neominalsolutions <neominal.solutions@gmail.com>
2023-07-21 18:52:57 +0300

    DenemeLib1 Package Import

---
+++ DenemeLib1/src/DenemeLib2/User.java
@@ -0,0 +1,19 @@
+package DenemeLib2;
+
+public class User {
+
+	private String name;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
--- DenemeLib1/src/Program/Main.java
+++ DenemeLib1/src/Program/Main.java
@@ -1,10 +1,19 @@
 package Program;
 
+import DenemeLib3.User;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		DenemeLib3.User usr = new DenemeLib3.User("ali");
+		System.out.println(usr.getName());
+		
+		DenemeLib2.User usr1 = new DenemeLib2.User();
+		usr1.setName("can");
+		System.out.println(usr1.getName());
 	}
 
 }
